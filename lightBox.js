6ca6847d1f9fd123eb39b359766d701a07db4383
Endy Tang <endytang0310@gmail.com>
2022-11-02 14:20:30 +0000

    topButton add animation & color

--- lightBox.js
+++ lightBox.js
@@ -1,87 +1,80 @@
-var colorArr = ["black" , "red" , "green" , "blue", "orange", "purple"]
+var colorArr = ["black", "red", "green", "blue", "orange", "purple"]
 
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+var text = document.getElementById("text")
+var move = document.getElementById("move")
+
+move.onclick = function() {
+  var word = "light Box";
+  for (var i = 0; i < colorArr.length; i++) {
+    var loopColor = colorArr[i]
+    if (text.classList.contains("text-color-" + loopColor)) {
+      text.classList.remove("text-color-" + loopColor)
+      console.log("test 1")
+    }
+  }
+  var randomColor = "text-color-" + getRandomColor()
+  text.classList.add(randomColor);
+  console.log("test 2")
+  if (!text.classList.contains("animation")) {
+    text.classList.add("animation")
+    text.innerHTML = word
+  }
 }
 
 
-function getRandomColor() {
-  var randomNumber = getRandomInteger(0,colorArr.length) //0,1,2,3
-  return colorArr[randomNumber]
-}
 
 
-var blueColor = document.getElementsByClassName("background-color-blue")
-var greenColor = document.getElementsByClassName("background-color-green")
-var blackColor = document.getElementsByClassName("background-color-black")
-
-
-var gridButtons = document.getElementsByClassName("grid-button")
-
-var topOne = document.getElementById("topOne");
-var number = document.getElementById("number"
-)
-topOne.onclick = function(){
-  var next;
-  if (number.innerHTML === '') {
-    next = 0
-  } else {
-    next = Number(number.innerHTML) + 1
-  }
-  number.innerHTML = next
-
-  //step 1 - Remove all classes background-color-*
- for (var i =0; i < colorArr.length; i++) {
-   var loopColor = colorArr[i]
-   if(topOne.classList.contains("background-color-" + loopColor)){
-     topOne.classList.remove("background-color-" + loopColor)
-   }
- }
-
-  //step 2 - Add random color class
-  var randomColor = "background-color-" + getRandomColor()
-  topOne.classList.add(randomColor) ;
-}
-
-var topTwo = document.getElementById("topTwo");
-
-topTwo.onclick = function() {
-  var next;
-  if (numberOne.innerHTML === '') {
-    next = 0
-  } else {
-    next = Number(numberOne.innerHTML) + 1
-  }
-  numberOne.innerHTML = next
-  if (topTwo.classList.contains("background-color-green")) {
-    console.log("check 1")
-    topTwo.classList.add("background-color-red")
-    topTwo.classList.remove("background-color-green")
-    return ;
+  function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  if (topTwo.classList.contains("background-color-red")) {
-    console.log("check 2")
-    topTwo.classList.add("background-color-black")
-    topTwo.classList.remove("background-color-red")
-    return;
+  function getRandomColor() {
+    var randomNumber = getRandomInteger(0, colorArr.length) //0,1,2,3
+    return colorArr[randomNumber]
   }
 
-  if (topTwo.classList.contains("background-color-black")) {
-    console.log("check 3")
-    topTwo.classList.add("background-color-blue")
-    topTwo.classList.remove("background-color-black")
-    return;
+  var topOne = document.getElementById("topOne");
+  var number = document.getElementById("number"
+  )
+  topOne.onclick = function () {
+    var next;
+    if (number.innerHTML === '') {
+      next = 1
+    } else {
+      next = Number(number.innerHTML) + 1
+    }
+    number.innerHTML = next
+
+    //step 1 - Remove all classes background-color-*
+    for (var i = 0; i < colorArr.length; i++) {
+      var loopColor = colorArr[i]
+      if (topOne.classList.contains("background-color-" + loopColor)) {
+        topOne.classList.remove("background-color-" + loopColor)
+      }
+    }
+    //step 2 - Add random color class
+    var randomColor = "background-color-" + getRandomColor()
+    topOne.classList.add(randomColor);
   }
 
-  if (topTwo.classList.contains("background-color-blue")) {
-    console.log("check 4")
-    topTwo.classList.add("background-color-green")
-    topTwo.classList.remove("background-color-blue")
-    return;
+  var topTwo = document.getElementById("topTwo");
+
+  topTwo.onclick = function () {
+    var next;
+    if (numberOne.innerHTML === '') {
+      next = 1
+    } else {
+      next = Number(numberOne.innerHTML) + 1
+    }
+    numberOne.innerHTML = next
+    for (var i = 0; i < colorArr.length; i++) {
+      var loopColor = colorArr[i]
+      if (topTwo.classList.contains("background-color-" + loopColor)) {
+        topTwo.classList.remove("background-color-" + loopColor)
+      }
+    }
+    //step 2 - Add random color class
+    var randomColor = "background-color-" + getRandomColor()
+    topTwo.classList.add(randomColor);
   }
-
-  topTwo.classList.add("background-color-green")
-
-}
 
